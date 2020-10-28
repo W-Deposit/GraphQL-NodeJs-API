d759b19f2c182f5fa73e69fba5c27f99cbb22952
bvenceslas <bvenceslas@gmail.com>
2020-10-28 22:50:29 +0200

    Add Open Password

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 const User = require("../models/User.model");
 // const { registerValidation } = require("../src/validation");
@@ -9,16 +11,27 @@ router.get('/data', (req, res) => {
 
 router.post("/register", async (req, res) => {
   //Validate the data with joi before send it to the database
-  
+  // const password = req.body.password;
+
+  // bcrypt.genSalt(10, (err, salt) => {
+  //   if(err) throw err;
+  //   bcrypt.hash(password,salt, (err, hash) => {
+  //     if(err) throw err;
+  //     passwordHashed = hash;
+  //   });
+  // })
   
   const user = new User({
     name: req.body.name,
     firstname: req.body.firstname,
     lastname: req.body.lastname,
+    username: req.body.username,
+    password: req.body.password,
     gender: req.body.gender,
     phoneNumber: req.body.phoneNumber,
     email: req.body.email,
   });
+
   try {
     const saveUserDetails = await user.save();
     res.send(saveUserDetails);
