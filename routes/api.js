92e1375475cfb0340e19db314381bace46b0bbfe
bvenceslas <bvenceslas@gmail.com>
2020-10-28 22:22:00 +0200

    Remove Joi and Test

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const User = require("../models/User.model");
-const { registerValidation } = require("../validation");
+// const { registerValidation } = require("../src/validation");
 
 router.get('/data', (req, res) => {
   res.send('I am Working properly ...');
@@ -9,8 +9,8 @@ router.get('/data', (req, res) => {
 
 router.post("/register", async (req, res) => {
   //Validate the data with joi before send it to the database
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  
+  
   const user = new User({
     name: req.body.name,
     firstname: req.body.firstname,
--- src/validation.js
+++ src/validation.js
@@ -6,13 +6,9 @@ const registerValidation = (data) => {
     name: Joi.string().min(6).required(),
     firstname: Joi.string().min(6).required(),
     lastname: Joi.string().min(6).required(),
-    phoneNumber: joi
-      .string()
-      .length(10)
-      .pattern(/^[0-9]+$/)
-      .required(),
+    phoneNumber: joi.string().length(10).pattern(/^[0-9]+$/).required(),
     email: Joi.string().min(6).required().email(),
   };
   return Joi.validate(data, schema);
 };
-module.exports.registerValidation = registerValidation;
+module.exports = registerValidation;
